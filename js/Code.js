c62aa290fdd95a72cc551c0906f39a3527e96403
arpitchaudhary <92873915+arpit9460216600@users.noreply.github.com>
2022-04-30 19:37:07 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "HAPPY";
+const accessCode2 = "MARRIAGE";
+const accessCode3 = "ANNIVERSARY";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: write all letters in capital Happy here!!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: write marriage !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: write anniversary !!", 100,270)
+
+}
